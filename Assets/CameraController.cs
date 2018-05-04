ff21c174f3f15ad77cd9877d9f55951000d6ac2f
Adelson Pacheco <adelsonpdr@hotmail.com>
2018-05-03 23:44:13 -0300

    engenharia reversa do mario

---
+++ Assets/CameraController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform target;
+	public float leftMargin;
+	public float rightMargin;
+	public float topMargin;
+	public float bottomMargin;
+	public float topLimit;
+	public float bottomLimit;
+	public Renderer backgroundRenderer;
+	public Transform backgroundTransform;
+	public float backgroundSpeed;
+
+	void Update () {
+
+		Vector3 newPosition;
+
+		newPosition = transform.position;
+
+		if ( target.position.x > transform.position.x + rightMargin )
+			newPosition.x = target.position.x - rightMargin;
+
+		if ( target.position.x < transform.position.x - leftMargin )
+			newPosition.x = target.position.x + leftMargin;
+
+		if ( target.position.y > transform.position.y + topMargin )
+			newPosition.y = target.position.y - topMargin;
+
+		if ( target.position.y < transform.position.y - bottomMargin )
+			newPosition.y = target.position.y + bottomMargin;
+
+		newPosition.y = Mathf.Clamp(newPosition.y, bottomLimit, topLimit);
+
+		backgroundTransform.position = new Vector2(newPosition.x, backgroundTransform.position.y);
+
+		backgroundRenderer.material.mainTextureOffset = new Vector2(newPosition.x * backgroundSpeed, 0);
+
+		transform.position = newPosition;
+
+	}
+
+}
---
+++ Assets/CatController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	public float speed;
+	public float gravity;
+	private Animator anim;
+	private SpriteRenderer sprite;
+	private bool jump;
+	public bool grounded;
+	public float impulse;
+	private int jumpCount;
+	public Collider2D feetCollider;
+
+	void Start () {
+
+		rb = GetComponent<Rigidbody2D>();
+
+		anim = GetComponent<Animator>();
+
+		sprite = GetComponent<SpriteRenderer>();
+
+		jumpCount = 0;
+
+	}
+
+	void Update () {
+
+		anim.SetFloat("speed", rb.velocity.x / speed);
+
+		anim.SetFloat("vspeed", rb.velocity.y);
+
+		if ( rb.velocity.x < -0.1f )
+			sprite.flipX = true;
+
+		if ( rb.velocity.x > 0.1f )
+			sprite.flipX = false;
+
+		grounded = feetCollider.IsTouchingLayers(1 << 8);
+
+		anim.SetBool("grounded", grounded);
+
+		if ( grounded && Input.GetButtonDown("Jump") ) {
+			jump = true;
+			jumpCount = 0;
+		} else if ( jumpCount < 1 && Input.GetButtonDown("Jump") ) {
+			jump = true;
+			jumpCount++;
+		}
+
+	}
+
+	void FixedUpdate() {
+
+		Vector2 newVelocity;
+
+		newVelocity = rb.velocity;
+
+		newVelocity.x = Input.GetAxis("Horizontal") * speed;
+
+		if ( jump ) {
+
+			newVelocity.y = impulse;
+
+			jump = false;
+				
+		}
+
+		newVelocity.y = Mathf.Clamp(newVelocity.y, -impulse, impulse);
+
+		rb.velocity = newVelocity;
+
+		rb.AddForce(new Vector2(0, -gravity));
+
+		if ( Input.GetButton("Jump") )
+			rb.AddForce(new Vector2(0, gravity/2));  
+
+	}
+
+}
